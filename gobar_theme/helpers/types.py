07002ec1f57f1ffe3ae7ee34cc803622423878ff
JairoJS1 <jairo.j199912@gmail.com>
2022-10-18 08:19:12 -0600

    Se agregan APIs para los recursos de un dataset

--- gobar_theme/helpers/types.py
+++ gobar_theme/helpers/types.py
@@ -60,7 +60,7 @@ def field_types(field_type_id=None):
 def distribution_types(distribution_type_id=None):
     types = [
         ("file", u"Archivo de datos"),
-   
+         ("api", u"API"),
         ("documentation", u"Documentación")
     ]
 
